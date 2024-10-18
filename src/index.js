e54c7263330227cc55a2af9941dfc341f5469ecf
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-10-18 18:02:48 -0300

    feat: add index to star

--- src/index.js
+++ src/index.js
@@ -2,14 +2,13 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import StarRating from "./StarRating";
-// import App from './App'
 
 function Test() {
   const [movieRating, setMovieRating] = useState(0);
   return (
     <div>
       <StarRating color="#eb87ce" onSetRating={setMovieRating} />
-      <p> Este filme foi clasiificado com {movieRating} estrelas</p>
+      <h3> Este filme foi clasiificado com {movieRating} estrelas</h3>
     </div>
   );
 }
@@ -17,13 +16,18 @@ function Test() {
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
     <StarRating
       maxRating={5}
       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
     />
+    <br />
+    <hr /> <br />
     <StarRating maxRating={5} size={25} color="#87ceeb" />
+    <br />
+    <hr /> <br />
     <StarRating maxRating={10} size={30} color="red" />
+    <br />
+    <hr /> <br />
     <StarRating
       maxRating={5}
       size={30}
@@ -31,6 +35,8 @@ root.render(
       className="test"
       defaultRating={3}
     />
+    <br />
+    <hr /> <br />
     <Test />
   </React.StrictMode>
 );
